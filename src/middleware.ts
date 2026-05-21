b90376bd0ec4381680b73f566a6f43e369a6587f
pabloArce2 <pabloarceproyects@gmail.com>
2026-05-21 12:31:45 +0200

    Test for supabase

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,6 +8,8 @@ export async function middleware(request: NextRequest) {
         },
     })
 
+    //Test for supabase *
+
     const supabase = createServerClient(
         process.env.NEXT_PUBLIC_SUPABASE_URL ?? "",
         process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? "",
